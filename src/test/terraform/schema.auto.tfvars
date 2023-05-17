7a5c6eb5cbf0aa28db16658aa955d8ffecba370f
Nathan Voxland <nathan@voxland.net>
2023-05-17 16:15:23 -0500

    Moved githubaction.sh into github lth.yml

--- src/test/terraform/schema.auto.tfvars
+++ src/test/terraform/schema.auto.tfvars
@@ -1,3 +1,3 @@
-TEST_CATALOG = "main"
-TEST_SCHEMA = "lb_test_harness"
+TF_VAR_TEST_CATALOG = "main"
+TF_VAR_TEST_SCHEMA = "lb_test_harness"
 schema_force_destroy = true
--- src/test/terraform/schema.tf
+++ src/test/terraform/schema.tf
@@ -1,11 +1,11 @@
-variable "TEST_SCHEMA" {
+variable "TF_VAR_TEST_SCHEMA" {
   description = "Name of Liquibase test harness database"
   type = string
   default = "lb_test_harness"
 }
 
 
-variable "TEST_CATALOG" {
+variable "TF_VAR_TEST_CATALOG" {
   description = "Catalog of liquibase testing database"
   type = string
   default = "main"
@@ -17,8 +17,8 @@ variable "schema_force_destroy" {
 }
 
 resource "databricks_schema" "test_harness" {
-  catalog_name = var.TEST_CATALOG
-  name = var.TEST_SCHEMA
+  catalog_name = var.TF_VAR_TEST_CATALOG
+  name = var.TF_VAR_TEST_SCHEMA
   comment = "This database is for liquibase test harness"
   force_destroy = var.schema_force_destroy
   properties = {
