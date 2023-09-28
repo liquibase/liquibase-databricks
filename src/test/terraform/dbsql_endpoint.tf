10896c42b5c5fe0c20ae4141a3efa9bf6ae4b2f0
CodyAustinDavis <cody.davis@databricks.com>
2023-09-28 08:04:35 -0700

    update dbsql creation terrafor for testing

--- src/test/terraform/dbsql_endpoint.tf
+++ src/test/terraform/dbsql_endpoint.tf
@@ -1,10 +1,9 @@
-data "databricks_current_user" "me" {}
-
 resource "databricks_sql_endpoint" "this" {
   name             = "Databricks Liquibase Test Harness Endpoint"
   cluster_size     = "Small"
   max_num_clusters = 1
   warehouse_type = "PRO"
+  force_destroy = true
 }
 
 output "endpoint_url" {
