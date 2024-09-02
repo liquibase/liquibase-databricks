561ed62af1b409ef97d911b4d5cad00bb847011d
Alejandro Alvarez <jandroav@icloud.com>
2024-09-02 14:22:53 +0200

    🔧 (lth.yml): update terraform import command to import specific resource before tainting another resource for better resource management 🔧 (dbsql_endpoint.tf): add random suffix to the name of the databricks_sql_endpoint resource to ensure uniqueness and improve resource identification

--- src/test/terraform/dbsql_endpoint.tf
+++ src/test/terraform/dbsql_endpoint.tf
@@ -1,8 +1,16 @@
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
 resource "databricks_sql_endpoint" "this" {
-  name             = "Databricks Liquibase Test Harness Endpoint"
+  name             = "Databricks Liquibase Test Harness Endpoint-${random_string.suffix.result}"
   cluster_size     = "Small"
   max_num_clusters = 1
   warehouse_type   = "PRO"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 output "endpoint_url" {
