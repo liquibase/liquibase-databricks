8d51e59da83b81187dbac49589c44ed6a94af5c5
Alejandro Alvarez <jandroav@icloud.com>
2024-09-02 14:14:01 +0200

    🔧 (lth.yml): add terraform taint commands to target specific resources before applying changes for better control and accuracy 🔧 (dbsql_endpoint.tf): remove random suffix generation for endpoint name to simplify and standardize naming convention

--- src/test/terraform/dbsql_endpoint.tf
+++ src/test/terraform/dbsql_endpoint.tf
@@ -1,16 +1,8 @@
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
 resource "databricks_sql_endpoint" "this" {
-  name             = "Databricks Liquibase Test Harness Endpoint-${random_string.suffix.result}"
+  name             = "Databricks Liquibase Test Harness Endpoint"
   cluster_size     = "Small"
   max_num_clusters = 1
   warehouse_type   = "PRO"
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 output "endpoint_url" {
