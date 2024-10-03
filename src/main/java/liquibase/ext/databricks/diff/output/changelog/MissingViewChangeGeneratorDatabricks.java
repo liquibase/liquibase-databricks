7d8ae2f6603899307c4e1bddc2bcd508c0a5a6fe
filipe <flautert@liquibase.org>
2024-10-03 17:20:42 -0300

    feat: completed view snapshot + code refactoring for class ViewSnapshotGeneratorDatabricks

--- src/main/java/liquibase/ext/databricks/diff/output/changelog/MissingViewChangeGeneratorDatabricks.java
+++ src/main/java/liquibase/ext/databricks/diff/output/changelog/MissingViewChangeGeneratorDatabricks.java
@@ -8,14 +8,17 @@ import liquibase.diff.output.changelog.ChangeGeneratorChain;
 import liquibase.diff.output.changelog.core.MissingViewChangeGenerator;
 import liquibase.ext.databricks.change.createView.CreateViewChangeDatabricks;
 import liquibase.ext.databricks.database.DatabricksDatabase;
-import liquibase.ext.databricks.structure.core.ViewDatabricks;
 import liquibase.structure.DatabaseObject;
+import liquibase.structure.core.View;
 
+/**
+ * Custom implementation of {@link MissingViewChangeGenerator} for Databricks.
+ */
 public class MissingViewChangeGeneratorDatabricks extends MissingViewChangeGenerator {
 
     @Override
     public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
-        if (database instanceof DatabricksDatabase && ViewDatabricks.class.isAssignableFrom(objectType)) {
+        if (database instanceof DatabricksDatabase && View.class.isAssignableFrom(objectType)) {
             return PRIORITY_DATABASE;
         } else {
             return PRIORITY_NONE;
@@ -28,7 +31,11 @@ public class MissingViewChangeGeneratorDatabricks extends MissingViewChangeGener
         if (changes == null || changes.length == 0) {
             return changes;
         }
+        changes[0] = getCreateViewChangeDatabricks(missingObject.getAttribute("tblProperties", String.class), changes);
+        return changes;
+    }
 
+    private CreateViewChangeDatabricks getCreateViewChangeDatabricks(String tblProperties, Change[] changes) {
         CreateViewChange temp = (CreateViewChange) changes[0];
         CreateViewChangeDatabricks createViewChangeDatabricks = new CreateViewChangeDatabricks();
         createViewChangeDatabricks.setViewName(temp.getViewName());
@@ -41,10 +48,8 @@ public class MissingViewChangeGeneratorDatabricks extends MissingViewChangeGener
         createViewChangeDatabricks.setPath(temp.getPath());
         createViewChangeDatabricks.setRelativeToChangelogFile(temp.getRelativeToChangelogFile());
         createViewChangeDatabricks.setEncoding(temp.getEncoding());
-        createViewChangeDatabricks.setTblProperties(((ViewDatabricks)missingObject).getTblProperties());
-        changes[0] = createViewChangeDatabricks;
-
-        return changes;
+        createViewChangeDatabricks.setTblProperties(tblProperties);
+        return createViewChangeDatabricks;
     }
 
     @Override
