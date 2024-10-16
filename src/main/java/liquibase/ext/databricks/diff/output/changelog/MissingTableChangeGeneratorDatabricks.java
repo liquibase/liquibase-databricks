065dbe41a4e3d3f6902c8880ace24e0940fa3389
KushnirykOleh <kushnirykOleh@gmail.com>
2024-10-16 18:19:59 +0300

    [DAT-18788] adding Location to snapshot, but not to generated changelog (#200)
    * wip
    
    * reverted ColumnSnapshotGeneratorDatabricks, it went to DAT-18790
    
    * wip
    
    * added missingTable generator
    
    * addressed PR comment
    
    * resolved Sonar issues
    
    * fixed exception when DBCL is missing
    
    * adding location only if type=External
    
    * removed location from changelog, but always have it in snapshot
    
    * removed unused fields, updated test

--- src/main/java/liquibase/ext/databricks/diff/output/changelog/MissingTableChangeGeneratorDatabricks.java
+++ src/main/java/liquibase/ext/databricks/diff/output/changelog/MissingTableChangeGeneratorDatabricks.java
@@ -31,8 +31,9 @@ public class MissingTableChangeGeneratorDatabricks extends MissingTableChangeGen
         if (changes == null || changes.length == 0) {
             return changes;
         }
+        //so far we intentionally omit tableLocation in generated changelog
         ExtendedTableProperties extendedTableProperties = new ExtendedTableProperties(
-                missingObject.getAttribute("Location", String.class),
+                null,
                 missingObject.getAttribute("tblProperties", String.class));
 
         changes[0] = getCreateTableChangeDatabricks(extendedTableProperties, changes);
