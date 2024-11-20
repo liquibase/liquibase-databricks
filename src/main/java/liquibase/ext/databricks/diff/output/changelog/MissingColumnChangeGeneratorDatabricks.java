2d094f9e4305ac8a631cafd265ef5be00213eb02
Mykhailo Savchenko <msavchenko@liquibase.com>
2024-11-20 15:18:30 +0200

    DAT-19050: refactored, added not null constraint handling as it was found it fails with similar error as default value for existing tables.

--- src/main/java/liquibase/ext/databricks/diff/output/changelog/MissingColumnChangeGeneratorDatabricks.java
+++ src/main/java/liquibase/ext/databricks/diff/output/changelog/MissingColumnChangeGeneratorDatabricks.java
@@ -2,8 +2,10 @@ package liquibase.ext.databricks.diff.output.changelog;
 
 import liquibase.change.AddColumnConfig;
 import liquibase.change.Change;
+import liquibase.change.ConstraintsConfig;
 import liquibase.change.core.AddColumnChange;
 import liquibase.change.core.AddDefaultValueChange;
+import liquibase.change.core.AddNotNullConstraintChange;
 import liquibase.database.Database;
 import liquibase.diff.output.DiffOutputControl;
 import liquibase.diff.output.changelog.ChangeGeneratorChain;
@@ -12,12 +14,13 @@ import liquibase.ext.databricks.database.DatabricksDatabase;
 import liquibase.statement.DatabaseFunction;
 import liquibase.structure.DatabaseObject;
 import liquibase.structure.core.Column;
+import org.apache.commons.lang3.ObjectUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * Custom diff change generator for Databricks
@@ -35,53 +38,105 @@ public class MissingColumnChangeGeneratorDatabricks extends MissingColumnChangeG
     @Override
     public Change[] fixMissing(DatabaseObject missingObject, DiffOutputControl control, Database referenceDatabase, Database comparisonDatabase, ChangeGeneratorChain chain) {
         Change[] changes = super.fixMissing(missingObject, control, referenceDatabase, comparisonDatabase, chain);
-        changes = handleColumnWithDefaultValues((Column) missingObject, control, changes);
+        changes = handleMissingColumnConstraints((Column) missingObject, control, changes);
         return changes;
     }
 
-    private Change[] handleColumnWithDefaultValues(Column column, DiffOutputControl control, Change[] changes) {
-        if(column.getDefaultValue() != null) {
-            Optional<AddColumnChange> addColumnOptional = Arrays.stream(changes)
-                    .filter(change -> isCurrentColumnChange(change, column, control))
-                    .map(AddColumnChange.class::cast).findFirst();
-            if(addColumnOptional.isPresent()) {
-                AddColumnChange addColumnChange = addColumnOptional.get();
-                AddColumnConfig addColumnConfig = addColumnChange.getColumns().get(0);
-                String defaultValue = addColumnConfig.getDefaultValue();
-                String columnDataType = addColumnConfig.getType();
-                Boolean isComputed = addColumnConfig.getComputed();
-                if(defaultValue != null) {
-                    List<Change> changeList = Arrays.asList(changes);
-                    //Filter out add column with default value to split it onto 2 changes
-                    changeList = changeList.stream().filter(change -> !isCurrentColumnChange(change, column, control))
-                            .collect(Collectors.toList());
-                    addColumnConfig.setDefaultValue(null);
-                    addColumnConfig.setDefaultValueComputed(null);
-                    addColumnConfig.setComputed(null);
-                    AddDefaultValueChange addDefaultValueChange = new AddDefaultValueChange();
-                    if (control.getIncludeCatalog()) {
-                        addDefaultValueChange.setCatalogName(column.getRelation().getSchema().getCatalog().getName());
-                    }
-                    if (control.getIncludeSchema()) {
-                        addDefaultValueChange.setSchemaName(column.getRelation().getSchema().getName());
-                    }
-                    addDefaultValueChange.setTableName(column.getRelation().getName());
-                    addDefaultValueChange.setColumnName(column.getName());
-                    addDefaultValueChange.setColumnDataType(columnDataType);
-
-                    if (isComputed != null && isComputed) {
-                        addDefaultValueChange.setDefaultValueComputed(new DatabaseFunction(defaultValue));
-                    } else {
-                        addDefaultValueChange.setDefaultValue(defaultValue);
-                    }
-                    addDefaultValueChange.setDefaultValueConstraintName(column.getDefaultValueConstraintName());
-                    changeList.add(addColumnChange);
-                    changeList.add(addDefaultValueChange);
-                    changes = changeList.toArray(new Change[0]);
-                }
-            }
+    private Change[] handleMissingColumnConstraints(Column column, DiffOutputControl control, Change[] changes) {
+        Optional<AddColumnChange> addColumnOptional = Arrays.stream(changes)
+                .filter(change -> isCurrentColumnChange(change, column, control))
+                .map(AddColumnChange.class::cast).findFirst();
+        if(addColumnOptional.isPresent()) {
+            AddColumnChange addColumnChange = addColumnOptional.get();
+            changes = splitAddColumnChange(column, control, changes, addColumnChange);
         }
         return changes;
+    }
+
+    private Change[] splitAddColumnChange(Column column, DiffOutputControl control, Change[] changes, AddColumnChange addColumnChange) {
+        List<Change> changeList = new ArrayList<>(Arrays.asList(changes));
+        AddColumnConfig addColumnConfig = addColumnChange.getColumns().get(0);
+        if(addColumnConfig.getDefaultValue() != null || addColumnConfig.getDefaultValueComputed() != null) {
+            AddDefaultValueChange addDefaultValueChange = handleDefaultValue(column, control, addColumnChange);
+            changeList.add(addDefaultValueChange);
+        }
+        if(addColumnConfig.getConstraints() != null && Objects.equals(addColumnConfig.getConstraints().isNullable(), Boolean.FALSE)) {
+            AddNotNullConstraintChange addNotNullConstraintChange = handleNotNull(column, control, addColumnChange);
+            changeList.add(addNotNullConstraintChange);
+        }
+        if(constraintsAreEmpty(addColumnConfig, addColumnConfig.getConstraints())) {
+            addColumnConfig.setConstraints(null);
+        }
+        changes = changeList.toArray(new Change[0]);
+        return changes;
+    }
+
+    private AddDefaultValueChange handleDefaultValue(Column column, DiffOutputControl control, AddColumnChange addColumnChange) {
+        AddColumnConfig addColumnConfig = addColumnChange.getColumns().get(0);
+        String defaultValue = addColumnConfig.getDefaultValue();
+        DatabaseFunction defaultValueComputed = addColumnConfig.getDefaultValueComputed();
+        String columnDataType = addColumnConfig.getType();
+        addColumnConfig.setDefaultValue(null);
+        addColumnConfig.setDefaultValueComputed(null);
+        addColumnConfig.setComputed(null);
+        AddDefaultValueChange addDefaultValueChange = new AddDefaultValueChange();
+        if (control.getIncludeCatalog()) {
+            addDefaultValueChange.setCatalogName(column.getRelation().getSchema().getCatalog().getName());
+        }
+        if (control.getIncludeSchema()) {
+            addDefaultValueChange.setSchemaName(column.getRelation().getSchema().getName());
+        }
+        addDefaultValueChange.setTableName(column.getRelation().getName());
+        addDefaultValueChange.setColumnName(column.getName());
+        addDefaultValueChange.setColumnDataType(columnDataType);
+
+        if (defaultValueComputed != null) {
+            addDefaultValueChange.setDefaultValueComputed(defaultValueComputed);
+        } else {
+            addDefaultValueChange.setDefaultValue(defaultValue);
+        }
+        addDefaultValueChange.setDefaultValueConstraintName(column.getDefaultValueConstraintName());
+        return addDefaultValueChange;
+    }
+
+    private AddNotNullConstraintChange handleNotNull(Column column, DiffOutputControl control, AddColumnChange addColumnChange) {
+        AddColumnConfig addColumnConfig = addColumnChange.getColumns().get(0);
+        ConstraintsConfig constraints = addColumnConfig.getConstraints();
+        constraints.setNullable((Boolean) null);
+        constraints.setNullable((String) null);
+        constraints.setNotNullConstraintName(null);
+        AddNotNullConstraintChange addNotNullConstraintChange = createAddNotNullConstraintChange(addColumnConfig, constraints);
+        if (control.getIncludeCatalog()) {
+            addNotNullConstraintChange.setCatalogName(column.getRelation().getSchema().getCatalog().getName());
+        }
+        if (control.getIncludeSchema()) {
+            addNotNullConstraintChange.setSchemaName(column.getRelation().getSchema().getName());
+        }
+        addNotNullConstraintChange.setTableName(column.getRelation().getName());
+        return addNotNullConstraintChange;
+    }
+
+    private AddNotNullConstraintChange createAddNotNullConstraintChange(AddColumnConfig column, ConstraintsConfig constraints) {
+        AddNotNullConstraintChange addNotNullConstraintChange = new AddNotNullConstraintChange();
+        addNotNullConstraintChange.setColumnName(column.getName());
+        addNotNullConstraintChange.setColumnDataType(column.getType());
+        addNotNullConstraintChange.setValidate(constraints.getValidateNullable());
+        addNotNullConstraintChange.setConstraintName(constraints.getNotNullConstraintName());
+        return addNotNullConstraintChange;
+    }
+
+    /**
+     * We perform reversed checks that were used in the
+     * {@link liquibase.change.core.AddColumnChange#generateStatements(Database)}
+     * to make sure there won't be empty constraints generated in generated changelog files.
+     * */
+    boolean constraintsAreEmpty(AddColumnConfig column, ConstraintsConfig constraints) {
+        if(constraints != null) {
+            return ObjectUtils.allNull(constraints.isNullable(), constraints.isUnique(), constraints.isPrimaryKey(),
+                    column.isAutoIncrement(), constraints.getReferences(), constraints.getReferencedColumnNames(),
+                    constraints.getReferencedTableName());
+        }
+        return column.isAutoIncrement() != null && !column.isAutoIncrement();
     }
 
     private boolean isCurrentColumnChange(Change change, Column currentColumn, DiffOutputControl control) {
