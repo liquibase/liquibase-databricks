f931d8fbcce947172763fafe046c8c2711e80b12
Charles Queiroz <fcqueiroz@liquibase.com>
2025-01-09 09:41:53 +0000

    DAT-19308 - To validate pipeline tests

--- src/main/java/liquibase/ext/databricks/sqlgenerator/CreateTableGeneratorDatabricks.java
+++ src/main/java/liquibase/ext/databricks/sqlgenerator/CreateTableGeneratorDatabricks.java
@@ -10,6 +10,7 @@ import liquibase.sql.UnparsedSql;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.sqlgenerator.core.CreateTableGenerator;
 import liquibase.statement.core.CreateTableStatement;
+import liquibase.structure.DatabaseObject;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
@@ -55,32 +56,27 @@ public class CreateTableGeneratorDatabricks extends CreateTableGenerator {
     public Sql[] generateSql(CreateTableStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
 
         Sql[] sqls = super.generateSql(statement, database, sqlGeneratorChain);
-        String baseSQL = sqls[0].toSql();
-        baseSQL = baseSQL.substring(0, baseSQL.lastIndexOf(")")) + ")";
-        StringBuilder finalsql = new StringBuilder(baseSQL);
+        StringBuilder finalsql = new StringBuilder(sqls[0].toSql());
+
+        if (statement instanceof CreateTableStatementDatabricks) {
         CreateTableStatementDatabricks thisStatement = (CreateTableStatementDatabricks) statement;
 
-        //if (statement instanceof CreateTableStatementDatabricks) {
-
-            if (!StringUtils.isEmpty(thisStatement.getTableFormat())) {
+            if ((!StringUtils.isEmpty(thisStatement.getTableFormat()))) {
                 finalsql.append(" USING ").append(thisStatement.getTableFormat());
             } else {
                 finalsql.append(" USING delta");
             }
-
-            if (thisStatement.getExtendedTableProperties() != null) {
-                String properties = processTableProperties(
-                        thisStatement.getExtendedTableProperties().getTblProperties()
-                );
-                finalsql.append(" TBLPROPERTIES (").append(properties).append(")");
+            if (thisStatement.getExtendedTableProperties() != null && StringUtils.isNotEmpty(thisStatement.getExtendedTableProperties().getTblProperties())) {
+                finalsql.append(" TBLPROPERTIES (").append(thisStatement.getExtendedTableProperties().getTblProperties()).append(")");
             } else {
-                String defaultProperties = processTableProperties("");
-                finalsql.append(" TBLPROPERTIES (").append(defaultProperties).append(")");
+                finalsql.append(" TBLPROPERTIES('delta.feature.allowColumnDefaults' = 'supported', 'delta.columnMapping.mode' = 'name', 'delta.enableDeletionVectors' = true)");
             }
 
             // Databricks can decide to have tables live in a particular location. If null, Databricks will handle the location automatically in DBFS
             if (!StringUtils.isEmpty(thisStatement.getTableLocation())) {
                 finalsql.append(" LOCATION '").append(thisStatement.getTableLocation()).append("'");
+            } else if (thisStatement.getExtendedTableProperties() != null && StringUtils.isNotEmpty(thisStatement.getExtendedTableProperties().getTableLocation())) {
+                finalsql.append(" LOCATION '").append(thisStatement.getExtendedTableProperties().getTableLocation()).append("'");
             }
 
             List<String> clusterCols = thisStatement.getClusterColumns();
@@ -88,54 +84,47 @@ public class CreateTableGeneratorDatabricks extends CreateTableGenerator {
 
 
             // If there are any cluster columns, add the clause
-            // ONLY if there are NO cluster columns, then do partitions, but never both.
+            // ONLY if there are NOT cluster columns, then do partitions, but never both.
             if (!clusterCols.isEmpty()) {
-                finalsql.append(" CLUSTER BY (")
-                        .append(String.join(", ", clusterCols))
-                        .append(")");
+
+                finalsql.append(" CLUSTER BY (");
+
+                int val = 0;
+                while (clusterCols.size() > val) {
+                    finalsql.append(clusterCols.get(val));
+
+                    val +=1;
+                    if (clusterCols.size() > val) {
+                        finalsql.append(", ");
+                    }
+                    else {
+                        finalsql.append(")");
+                    }
+                }
             } else if (!partitionCols.isEmpty()) {
-                finalsql.append(" PARTITIONED BY (")
-                        .append(String.join(", ", partitionCols))
-                        .append(")");
-            }
-      //  }
+                finalsql.append(" PARTITIONED BY (");
 
-        return new Sql[]{new UnparsedSql(finalsql.toString(), getAffectedTable(thisStatement))};
+                int val = 0;
+                while (partitionCols.size() > val) {
+                    finalsql.append(partitionCols.get(val));
+
+                    val +=1;
+                    if (partitionCols.size() > val) {
+                        finalsql.append(", ");
+                    }
+                    else {
+                        finalsql.append(")");
+                    }
+                }
     }
 
-    private String processTableProperties(String userProperties) {
-        // If there are no user properties, we return all essential properties with their default values
-        if (StringUtils.isEmpty(userProperties)) {
-            return ESSENTIAL_PROPERTIES.stream()
-                    .map(prop -> prop + " = " + DEFAULT_VALUES.get(prop))
-                    .collect(joining(", "));
+
         }
 
-        // convert user properties into a Map for processing
-        Map<String, String> properties = new LinkedHashMap<>();
+        sqls[0] = new UnparsedSql(finalsql.toString(), sqls[0].getAffectedDatabaseObjects().toArray(new DatabaseObject[0]));
 
-        if (!StringUtils.isEmpty(userProperties)) {
-            Arrays.stream(userProperties.split(","))
-                    .map(String::trim)
-                    .map(prop -> prop.split("="))
-                    .forEach(parts -> {
-                        String key = parts[0].trim();
-                        String value = parts[1].trim();
-                        properties.put(key, value);
-                    });
+        return sqls;
+
         }
 
-        // For each essential property, we check whether it exists
-        // If it doesn't exist, we add it with the default value
-        for (String essentialProp : ESSENTIAL_PROPERTIES) {
-            if (!properties.containsKey(essentialProp)) {
-                properties.put(essentialProp, DEFAULT_VALUES.get(essentialProp));
-            }
-        }
-
-        // convert the Map back to string
-        return properties.entrySet().stream()
-                .map(entry -> entry.getKey() + " = " + entry.getValue())
-                .collect(joining(", "));
-    }
 }
