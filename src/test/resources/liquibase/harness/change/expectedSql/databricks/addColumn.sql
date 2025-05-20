23bc56f7b07be61411dff2efaff07f4752621464
Mykhailo Savchenko <msavchenko@liquibase.com>
2025-05-20 13:36:22 +0300

    INT-1297: extended harness suites to cover comments with quotes handling

--- src/test/resources/liquibase/harness/change/expectedSql/databricks/addColumn.sql
+++ src/test/resources/liquibase/harness/change/expectedSql/databricks/addColumn.sql
@@ -6,3 +6,4 @@ UPDATE main.liquibase_harness_test_ds.authors SET stringColumn = 'INITIAL_VALUE'
 UPDATE main.liquibase_harness_test_ds.authors SET varcharColumn = 'INITIAL_VALUE'
 UPDATE main.liquibase_harness_test_ds.authors SET intColumn = 5
 UPDATE main.liquibase_harness_test_ds.authors SET dateColumn = '2023-09-21'
+ALTER TABLE main.liquibase_harness_test_ds.authors ALTER COLUMN stringColumn COMMENT 'Quite complicated remarks with "some text" double quoted and \'another\' single quoted'
