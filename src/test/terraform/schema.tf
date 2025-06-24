2859b4a32894e15916c6d80c529af0e35c1930fa
Sayali M <smohadikar@liquibase.com>
2025-06-24 16:13:13 -0500

    feat(terraform): add data source for current user and schema management grants

--- src/test/terraform/schema.tf
+++ src/test/terraform/schema.tf
@@ -16,6 +16,10 @@ variable "schema_force_destroy" {
   default = true
 }
 
+# Data source to retrieve information about the currently authenticated user or service principal
+# This will automatically detect whether authentication is via user account or service principal
+data "databricks_current_user" "me" {}
+
 resource "databricks_schema" "test_harness" {
   catalog_name  = var.TF_VAR_TEST_CATALOG
   name          = var.TF_VAR_TEST_SCHEMA
@@ -24,4 +28,28 @@ resource "databricks_schema" "test_harness" {
   properties = {
     purpose = "testing"
   }
+}
+
+# Grant specific schema-level permissions to current user/service principal
+# These permissions provide comprehensive management capabilities for the test schema:
+# - USE_SCHEMA: Allows viewing and using the schema
+# - CREATE_TABLE: Allows creating new tables within the schema
+# - MODIFY: Allows modifying existing tables and their data
+# - SELECT: Allows reading data from tables in the schema
+resource "databricks_grant" "schema_manage" {
+  schema = databricks_schema.test_harness.id
+
+  principal  = data.databricks_current_user.me.user_name
+  privileges = ["USE_SCHEMA", "CREATE_TABLE", "MODIFY", "SELECT"]
+}
+
+# Alternative grant providing ALL_PRIVILEGES (equivalent to MANAGE permission)
+# This grants the most comprehensive permission set possible on the schema
+# Use this if you need full administrative control over the schema
+# Note: You may choose to use either this grant OR the specific permissions above
+resource "databricks_grant" "schema_full_manage" {
+  schema = databricks_schema.test_harness.id
+
+  principal  = data.databricks_current_user.me.user_name
+  privileges = ["ALL_PRIVILEGES"]
 }
